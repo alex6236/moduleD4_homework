385e92b5b8a2b8f0aeea3ee34c6a8575422341a2
Aleksej Savica <sova11@mail.ru>
2023-07-04 11:52:16 +0300

    отправка ментору

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,13 @@
+# from django import views
 from django.urls import path
 from .views import NewsList, NewsDetail, LoremDetail, PostSearch
+from . import views
 
 urlpatterns = [
+   
     path('', NewsList.as_view(), name='news'),
-    path('news/<int:pk>/', LoremDetail.as_view(), name='lorem_post'),
-    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
-    path('search/', PostSearch.as_view(), name='search')
+    path('<int:pk>/', LoremDetail.as_view(), name='lorem_post'),
+    path('news/<int:pk>', NewsDetail.as_view(), name='news_detail'),
+    path('search/', PostSearch.as_view(), name='search'),
+    # path('search/', views.search, name='search'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render
 from datetime import datetime, timezone
 from django.views.generic import ListView, DetailView
+
 from .models import Post
 from .filters import PostFilter
 from .forms import DateFilterForm, TitleFilterForm, UsernameFilterForm
+from django.db.models import Q
 
 class NewsList(ListView):
     model = Post
@@ -32,14 +34,34 @@ class PostSearch(ListView):
     template_name = 'search.html'
     context_object_name = 'search'
     queryset = Post.objects.order_by('-id')
-    paginate_by = 10
+    paginate_by = 50
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        filter = PostFilter(self.request.GET, queryset=self.get_queryset())
+        if self.request.GET:
+            queryset = self.get_queryset()
+        else:
+            queryset = None
+        filter = PostFilter(self.request.GET, queryset)
+        # filter = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
         context['filter'] = filter
-        # context['form'] = filter.form
+        context['form'] = filter.form
         context['date'] = DateFilterForm(self.request.GET or None) 
         context['title'] = TitleFilterForm(self.request.GET or None) 
         context['username'] = UsernameFilterForm(self.request.GET or None) 
         return context
+    
+# def search(request):
+#     if 'q' in request.GET:
+#         q = request.GET['q']
+#         multiple_q = Q(Q(title__icontains = q) | Q(author__authorUser__username__icontains = q) | Q(text__icontains = q))
+#         data = Post.objects.filter(multiple_q)
+#     else:
+#         data = Post.objects.all()
+#     context = {
+#         'data': data
+#     }
+
+#     return render(request, 'news.html', context)
+
